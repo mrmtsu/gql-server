cd64699141101391bbb7b7b23b6d13c1e3dc0f95
mrmtsu <81202muramatsu@gmail.com>
2022-03-08 06:38:14 +0900

    ♻️ ドメインの定義

--- domain/entity/id.go
+++ domain/entity/id.go
@@ -5,3 +5,9 @@ import "github.com/google/uuid"
 func GenUuid() string {
 	return uuid.New().String()
 }
+
+type UserID string
+
+func (u UserID) String() string {
+	return string(u)
+}
---
+++ domain/entity/user.go
@@ -0,0 +1,21 @@
+package entity
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/guregu/null"
+)
+
+var (
+	_ = time.Second
+	_ = sql.LevelDefault
+	_ = null.Bool{}
+)
+
+type User struct {
+	ID        string    `gorm:"primary_key;column:id"`
+	Name      string    `gorm:"column:name"`
+	CreatedAt time.Time `gorm:"column:created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at"`
+}
